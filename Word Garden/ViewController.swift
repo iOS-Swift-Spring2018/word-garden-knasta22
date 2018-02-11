9570c0c534b9a2ddcac4c161cf5a273233e557c1
Kerry Nasta <KerryNasta@kerrys-mbp.bc.edu>
2018-02-11 14:03:09 -0500

    Nasta Word Garden 2

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -16,9 +16,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var guessCountLabel: UILabel!
     @IBOutlet weak var playAgainButton: UIButton!
     @IBOutlet weak var flowerImageView: UIImageView!
+    var wordToGuess = "CODE"
+    var lettersGuessed = ""
+    let maxNumberWrongGuesses = 8
+    var wrongGuessesRemaining = 8
+    var guessCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        formatUserGuessLabel()
         guessLetterButton.isEnabled = false
         playAgainButton.isHidden = true
     }
@@ -26,6 +32,53 @@ class ViewController: UIViewController {
     func updateUIAfterGuess() {
         guessedLetterField.resignFirstResponder()
         guessedLetterField.text = ""
+    }
+    
+    func formatUserGuessLabel() {
+        var revealedWord = ""
+        lettersGuessed += guessedLetterField.text!
+        
+        for letter in wordToGuess {
+            if lettersGuessed.contains(letter) {
+                revealedWord = revealedWord + " \(letter)"
+            } else {
+                revealedWord += " _"
+            }
+        }
+        revealedWord.removeFirst()
+        userGuessLabel.text = revealedWord
+    }
+    
+    func guessALetter() {
+        formatUserGuessLabel()
+        guessCount += 1
+        
+        //decrements the wrongGuessesRemaining and shows the next flower image with one less pedal
+        let currentLetterGuessed = guessedLetterField.text!
+        if !wordToGuess.contains(currentLetterGuessed) {
+            wrongGuessesRemaining = wrongGuessesRemaining - 1
+            flowerImageView.image = UIImage(named: "flower\(wrongGuessesRemaining)")
+        }
+        
+        let revealedWord = userGuessLabel.text!
+        //stop game if wrongGuessesRemaining = 0
+        if wrongGuessesRemaining == 0 {
+            playAgainButton.isHidden = false
+            guessedLetterField.isEnabled = false
+            guessLetterButton.isEnabled = false
+            guessCountLabel.text = "Sorry, all out of guesses. Try again?"
+        } else if !revealedWord.contains("_") {
+            //you've won a game
+            playAgainButton.isHidden = false
+            guessedLetterField.isEnabled = false
+            guessLetterButton.isEnabled = false
+            guessCountLabel.text = "You've got it! It took you \(guessCount) guesses to guess the word"
+        } else {
+            //update our guess count
+            let guess = (guessCount == 1 ? "guess" : "guesses" )
+            
+            guessCountLabel.text = "You've made \(guessCount) \(guess)"
+        }
     }
     
     @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
@@ -39,14 +92,25 @@ class ViewController: UIViewController {
     }
     
     @IBAction func doneKeyPressed(_ sender: UITextField) {
+        guessALetter()
         updateUIAfterGuess()
     }
     
     @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+        guessALetter()
         updateUIAfterGuess()
     }
     
     @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+        playAgainButton.isHidden = true
+        guessedLetterField.isEnabled = true
+        guessLetterButton.isEnabled = false
+        flowerImageView.image = UIImage(named: "flower8")
+        wrongGuessesRemaining = maxNumberWrongGuesses
+        lettersGuessed = ""
+        formatUserGuessLabel()
+        guessCountLabel.text = "You've made 0 guesses"
+        guessCount = 0
     }
     
 }
--- manipulating strings.playground/Contents.swift
+++ manipulating strings.playground/Contents.swift
@@ -11,6 +11,46 @@ str.last
 //print("str = \"\(str)\"")
 //print(str.last!)
 
-if let lastChar = str.last {
-    str = lastChar
+var myName = "Gallaugher"
+var substring = "laugh"
+if myName.contains(substring) {
+    print("my name \(myName) contains \(substring)")
+} else {
+    print("there's no \(substring) in \(myName)")
 }
+
+var occupation = "Swift developer"
+var searchString = "developer"
+print(occupation.hasPrefix(searchString))
+print(occupation.hasSuffix(searchString))
+
+occupation.removeFirst(searchString.count)
+print(occupation)
+
+var address = "123 James St."
+var searchFor = "St."
+var replaceWith = "Street"
+var standardAddress = address.replacingOccurrences(of: searchFor, with: replaceWith)
+print(address, standardAddress)
+
+var crazyCaps = "SwIFt DeVeLopER"
+var upperCase = crazyCaps.uppercased()
+var lowerCase = crazyCaps.lowercased()
+var wordCapitalized = crazyCaps.capitalized
+print(upperCase, lowerCase, wordCapitalized)
+print(crazyCaps)
+
+var wordToGuess = "SWIFT"
+var lettersGuessed = "XWSET"
+var revealedWord = ""
+
+for letter in wordToGuess {
+    if lettersGuessed.contains(letter) {
+        revealedWord = revealedWord + " \(letter)"
+    } else {
+        revealedWord += " _"
+    }
+}
+revealedWord.removeFirst()
+print(revealedWord)
+
